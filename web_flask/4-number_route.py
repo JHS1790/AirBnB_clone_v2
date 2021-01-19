5dc6df2e74e20ae6a9d47bb2383138defd4015d5
JHS1790 <1790@holbertonschool@gmail.com>
2021-01-19 17:36:38 +0000

    first go at 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """display “Hello HBNB!”"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display “HBNB””"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisnotfun(text):
+    """display “C ” followed by the value of the text variable”"""
+    text = text.replace("_", " ")
+    return 'C %s' % text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonisnotcool(text="is cool"):
+    """display “Python ”, followed by the value of the text variable”"""
+    text = text.replace("_", " ")
+    return 'Python %s' % text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def nsucks(n):
+    """display “n is a number” only if n is an integer”"""
+    return '%d is a number' % n
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
